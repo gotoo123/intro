d535115fb1e17aa6eb5dd1b9e5b7821401c06865
“gotoo123” <1219727892@qq.com>
2024-01-02 21:38:01 +0800

    feat: add header content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import './app.less';
 
 const App = () => {
-  return <div>hello React</div>
+  return <>
+    <div className={`intro-wrapper`}>
+      <div className={`intro-header`}>
+        <section className={`intro-title`}>
+          <div className={`intro-title-left`}>
+            <h1>GOTOO</h1>
+            <h3>Name</h3>
+          </div>
+          <div className={`intro-title-right`}>
+            <h1>前端开发工程师</h1>
+            <h5>/成都</h5>
+          </div>
+        </section>
+
+        <section className={`intro-info`}>
+          <div className={`intro-info-left`}>
+            <div>男 / 1999.10</div>
+            <div>本科 / 2021年毕业 · 3年工作经验</div>
+            <div>西安电子科技大学 · 计算机科学与技术</div>
+          </div>
+          <div className={`intro-info-right`}>
+            <div>18681952753</div>
+            <div>me@163.com</div>
+          </div>
+
+        </section>
+      </div>
+      <div className={`content`}>
+
+      </div>
+    </div>
+
+
+  </>
 }
 
 export default App
